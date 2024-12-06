584aa9f106b4ab0d492121a94db4b88edc4fad8f
Maxence <maxencekrill@gmail.com>
2024-12-06 04:07:48 +0100

    fix: Header

--- components/header/HeaderButton.tsx
+++ components/header/HeaderButton.tsx
@@ -9,6 +9,7 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import Link from "next/link";
 
 
 interface HeaderButtonProps {
@@ -19,7 +20,7 @@ export const HeaderButton: React.FC<HeaderButtonProps> = ({links}) => {
   return (
     <div className="flex gap-4">
       <AlertDialog>
-        <AlertDialogTrigger><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-align-justify"><path d="M3 12h18"/><path d="M3 18h18"/><path d="M3 6h18"/></svg></AlertDialogTrigger>
+        <AlertDialogTrigger><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="size-6"><path d="M3 12h18"/><path d="M3 18h18"/><path d="M3 6h18"/></svg></AlertDialogTrigger>
         <AlertDialogContent>
           <AlertDialogHeader>
             <AlertDialogTitle>Où voulez-vous aller ?</AlertDialogTitle>
@@ -28,8 +29,8 @@ export const HeaderButton: React.FC<HeaderButtonProps> = ({links}) => {
           <AlertDialogFooter>
             <AlertDialogCancel>Rester</AlertDialogCancel>
             {links.map((link) => (
-                <AlertDialogAction key={link}>
-                    <a href={link}>{link}</a>
+                <AlertDialogAction asChild key={link}>
+                  <Link href={link}>{link}</Link>
                 </AlertDialogAction>
             ))}
           </AlertDialogFooter>  
